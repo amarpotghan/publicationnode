fd3e905e850e7bfc1961693446b3ad0ae015742b
amar <amar@inbetween.co.in>
2013-05-20 14:08:37 +0530

    Added a custom angular directive for collapsable directory tree...

--- app/js/controllers/TreeController.js
+++ app/js/controllers/TreeController.js
@@ -1,3 +1,5 @@
+
+
 myApp.controller("TreeController", ['$scope','TreeService','FileService', function($scope,TreeService,FileService) {
 
     //DATAPROVIDER FOR ADDING THE TABS
@@ -6,6 +8,7 @@ myApp.controller("TreeController", ['$scope','TreeService','FileService', functi
     //DATAPROVIDER FOR ADDING THE TREE
     $scope.tree = TreeService.query(function(tree){
         $scope.tree = tree;
+        $scope.childVisible= false;
     });
 
     //FUNCTION FOR ADDING TAB DYNAMICALLY ON CLICK OF THE ANY FILE IN TREE
@@ -13,11 +16,13 @@ myApp.controller("TreeController", ['$scope','TreeService','FileService', functi
             if(data.type == 'file'){
                 data.fileContent = FileService.getFileData(data.path).query({path:data.path},function(fileContent){
                     data.fileContent = fileContent;
-
                     var tabService= new TabService($scope.panes.length,$scope.panes,data);
                     tabService.addTab();
 
                 });
+            }
+            else{
+                $scope.childVisible = true;
             }
     }
 
