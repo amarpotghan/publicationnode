35c5649768e29816010bd7dc6024199184b8da0e
amarpotghan <amarpotghan@gmail.com>
2013-05-23 18:15:38 +0530

    master ref removed

--- app.js
+++ app.js
@@ -62,28 +62,6 @@ app.use(function(req, res, next){
 // load controllers
 require('./lib/config/UrlDecoder')(app, { verbose: !module.parent });
 
-// assume "not found" in the error msgs
-// is a 404. this is somewhat silly, but
-// valid, you can do whatever you like, set
-// properties, use instanceof etc.
-/*
-app.use(function(err, req, res, next){
-    // treat as 404
-    if (~err.message.indexOf('not found')) return next();
-
-    // log it
-    console.error(err.stack);
-
-    // error page
-    res.status(500).render('5xx');
-});
-
-// assume 404 since no middleware responded
-app.use(function(req, res, next){
-    res.status(404).render('404', { url: req.originalUrl });
-});
-*/
-
 if (!module.parent) {
     app.listen(5000);
     console.log('\n  listening on port 5000\n');
